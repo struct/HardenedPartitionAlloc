4d52e1431600693189c39a93656f82cd139abc16
struct <chris.rohlf@gmail.com>
2016-09-06 22:38:50 -0400

    initial commit, this thing needs work

---
+++ Makefile
@@ -0,0 +1,28 @@
+## Chris Rohlf - 2016
+## chris.rohlf@gmail.com
+
+UNAME := $(shell uname)
+
+CXX = clang++
+
+ifeq ($(UNAME), Darwin)
+CXXFLAGS = -std=c++11 -Wall -pedantic -D_FORTIFY_SOURCE=2 -fPIC -fstack-protector-all -DENABLE_ASSERT=1 -framework CoreFoundation
+endif
+
+ifeq ($(UNAME), Linux)
+CXXFLAGS = -std=c++11 -Wall -pedantic -D_FORTIFY_SOURCE=2 -fPIC -fstack-protector-all -DENABLE_ASSERT=1
+endif
+
+DEBUG = -ggdb
+ASAN = -fsanitize=address
+LDFLAGS =
+
+library:
+	$(CXX) $(CXXFLAGS) $(DEBUG) AddressSpaceRandomization.cpp Assertions.cpp PageAllocator.cpp \
+		PartitionAlloc.cpp -shared $(LDFLAGS) -o build/partitionalloc.so
+
+test: library
+	$(CXX) $(CXXFLAGS) $(DEBUG) pa_test.cpp build/partitionalloc.so -o build/pa_test
+
+clean:
+	rm -rf */*.o build/*
---
+++ pa_test.cpp
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include "config.h"
+#include "PartitionAlloc.h"
+
+PartitionAllocatorGeneric my_partition;
+
+class MyClass : public PartitionBackedBase {
+  public:
+	MyClass() {
+		ptr = NULL;
+		idx = 0;
+	}
+
+	~MyClass() { }
+
+	void setPtr(char *s) {
+		ptr = s;
+	}
+	char *getPtr() {
+		return ptr;
+	}
+
+	void setIdx(int i) {
+		idx = i;
+	}
+
+	int getIdx() {
+		return idx;
+	}
+
+  private:
+	char *ptr;
+	int idx;
+};
+
+void run_test() {
+	// PartitionAlloc API test with global root
+	my_partition.init();
+	void *p = partitionAllocGeneric(my_partition.root(), 16);
+	partitionFreeGeneric(my_partition.root(), p);
+	my_partition.shutdown();
+
+	// These allocations will fail before 512
+	// is reached because we build in sizes
+	// for the canary value
+	for(int i = 0; i < 512; i++) {
+		p = partition_malloc_sz(64);
+		// We expect this allocation to fail at some point
+		// because of size+cookie > bucket size
+		partition_free_sz(p);
+	}
+
+	for(int i = 0; i < 512; i++) {
+		p = partition_malloc_sz(128);
+		// We expect this allocation to fail at some point
+		// because of size+cookie > bucket size
+		partition_free_sz(p);
+	}
+
+	for(int i = 0; i < 512; i++) {
+		p = partition_malloc_sz(256);
+		// We expect this allocation to fail at some point
+		// because of size+cookie > bucket size
+		partition_free_sz(p);
+	}
+
+	for(int i = 0; i < 512; i++) {
+		p = partition_malloc_sz(512);
+		// We expect this allocation to fail at some point
+		// because of size+cookie > bucket size
+		partition_free_sz(p);
+	}
+
+	p = partition_malloc_string(128);
+	ASSERT(p);
+	partition_free_string(p);
+
+	p = partition_malloc(512);
+	ASSERT(p);
+	partition_free(p);
+
+	// Create a new MyClass which inherits from PartitionBackedBase
+	// which overloads the new operator
+	MyClass *mc = new MyClass();
+	ASSERT(mc);
+	delete mc;
+
+	void *gp = new_generic_partition();
+	p = generic_partition_alloc(gp, 128);
+	ASSERT(p);
+	generic_partition_free(gp, p);
+	delete_generic_partition(gp);
+
+	// Allocate some memory with glibc ptmalloc2
+	void *t = malloc(1024);
+	ASSERT(t);
+	free(t);
+}
+
+int main(int argc, char *argv[]) {
+	// Initialize the C API by calling _init() which will
+	// make sure all generic partitions are initialized
+	partitionalloc_init();
+
+	// Run all tests
+	run_test();
+
+	// Shutdown all generic partitions
+	partitionalloc_shutdown();
+
+	return 0;
+}
